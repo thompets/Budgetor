0dad5041490c979d5da653b9865f87cf40a26f91
Pete Thompson <peter.thompson55@gmail.com>
2016-05-15 22:12:28 -0500

    Added simple views for later configuration, removed old files.

--- Budgetor.Web/App/viewmodels/about.js
+++ Budgetor.Web/App/viewmodels/about.js
@@ -1,1 +1,19 @@
-define({});
+(function() {
+    define(['durandal/system'], function(system) {
+        var vm = {};
+
+        vm.activate = function () {
+            system.log('** activate about');
+        };
+
+        vm.attached = function () {
+            system.log('** attached about');
+        };
+
+        vm.canActivate = function () {
+            return { redirect: '#catalog' };
+        };
+
+        return vm;
+    });
+})();
